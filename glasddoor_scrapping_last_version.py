da0ffeec744103952e10b18b04455c8208f52204
Mohammed Zwaib <46110270+Mohammed-Rwelly@users.noreply.github.com>
2022-08-13 01:53:50 -0700

    Update glasddoor_scrapping_last_version.py

--- glasddoor_scrapping_last_version.py
+++ glasddoor_scrapping_last_version.py
@@ -10,15 +10,14 @@ from selenium.webdriver.chrome.service import Service
 from datetime import datetime,timedelta
 import re
 #locations = ["Qatar","United Kingdom","France","Turkey"]
-locations = ["Qatar"]
-def get_jobs(keyword, num_jobs):
+locations = ["Qatar","United Kingdom"]
+def get_jobs(keyword, num_jobs, verbose,path):
     '''Gathers jobs as a dataframe, scraped from Glassdoor'''
     # Initializing the webdriver
     global jobs_for_country
     jobs_for_country = []
     global jobs_for_countries
     jobs_for_countries = []
-    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
     chrome_options = Options()
     options = [
     "--headless",
@@ -31,7 +30,7 @@ def get_jobs(keyword, num_jobs):
     for option in options:
        chrome_options.add_argument(option)
     options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    driver = webdriver.Chrome(executable_path=path , options=chrome_options)
     for country in locations:
         jobs_for_country=[]
         url='https://www.glassdoor.com/Search/results.htm?keyword={}&locT=C&locName={}'.format(keyword.replace(' ','%20'),country)
@@ -39,21 +38,18 @@ def get_jobs(keyword, num_jobs):
         driver.get(url)
         # Let the page load. Change this number based on your internet speed.
         # Maybe add extra sleeping at the steps you need for more loading time. 
-        time.sleep(5)
+        #time.sleep(1)
         #click on button search depends on job_title and city 
         try:
             driver.find_element(By.XPATH,'//span[@class="SVGInline d-flex white"]').click()
         except:
             pass
         #after loding the page we are clicking on "see all the jobs " button 
-        time.sleep(10)
+        time.sleep(5)
         try:
             driver.find_element(By.XPATH,'//span[@class="SVGInline css-1mgba7 css-1hjgaef"]').click()
         except:
             pass
-        
-       # time.sleep(5)
-       # time.sleep(5)
         while len(jobs_for_country) < num_jobs: 
             job_buttons = driver.find_elements(By.XPATH,"//*[@id='MainCol']/div[1]/ul/li")
             # Going through each job url in this page
@@ -66,23 +62,22 @@ def get_jobs(keyword, num_jobs):
                     # When the number of jobs collected has reached the number we set. 
                     break
                 job_buttons[job].click()  
-                time.sleep(5)
+                time.sleep(3)
                 try:
                     driver.find_element(By.XPATH,'//*[@id="JAModal"]/div/div[2]/span').click()
                 except NoSuchElementException:
-                    print("وينهاااااااا")
                     pass
                 collected_successfully = False
                                
                 while not collected_successfully:
                     try:
-                        time.sleep(10)
+                        #time.sleep(10)
                         company_name = driver.find_element(By.XPATH,'//div[@class="css-xuk5ye e1tk4kwz5"]').text
                         location = driver.find_element(By.XPATH,'.//div[@class="css-56kyx5 e1tk4kwz1"]').text
                         job_title = driver.find_element(By.XPATH,'.//div[@class="css-1j389vi e1tk4kwz2"]').text
                         job_id  = job_buttons[job].get_attribute("data-id")
                         job_url= job_buttons_href[job].get_attribute("href")
-                        job_description = driver.find_element(By.XPATH,'.//div[@class="jobDescriptionContent desc"]').text
+                        #job_description = driver.find_element(By.XPATH,'.//div[@class="jobDescriptionContent desc"]').text
                         collected_successfully = True
                     except:
                         collected_successfully = True
@@ -99,7 +94,7 @@ def get_jobs(keyword, num_jobs):
                     Posted_Date=driver.find_element(By.XPATH,'//*[@id="MainCol"]/div[1]/ul/li[{}]/div[2]/div[2]/div/div[2]'.format(job+1)).text
                     pass    
                 now = datetime.now()
-                print("yesssssssssssssssssss =",type(Posted_Date))
+                #print("yesssssssssssssssssss =",type(Posted_Date))
                 try:
                     exdate= [int(x) for x in re.findall(r'-?\d+\.?\d*',Posted_Date)][0]
                     Posted_Data_N=now.date() - timedelta(days=exdate)
@@ -120,13 +115,13 @@ def get_jobs(keyword, num_jobs):
                 except NoSuchElementException:
                     # You need to set a "not found value. It's important."
                     salary_estimate = 'N/A'
-                print("the salary is =",salary_estimate) 
+               # print("the salary is =",salary_estimate) 
                 try: 
                     rating = driver.find_element(By.XPATH,'//*[@id="employerStats"]/div[1]/div[1]').text
                 except NoSuchElementException:
                     # You need to set a "not found value. It's important."
                     rating = 'N/A'
-                print("the Rating = ",rating)
+                #print("the Rating = ",rating)
                
                 #Extract Current Date Collection from a Datetime Object
                 now = datetime.now()
@@ -144,21 +139,20 @@ def get_jobs(keyword, num_jobs):
                 "Salary" : salary_estimate,
                 "jobType" :job_type,
                 "JobURL" : job_url,
-                "ShortDiscribtion" : job_description,
+                "ShortDiscribtion" : "N/A",
                 "fullJobDescribtion":job_description_full,
                 "Posted_Date_N":Posted_Data_N,
                 })
             # Clicking on the "next page" button
             try:
                 driver.find_element(By.CSS_SELECTOR,'[alt="next-icon"]').click()
-               # time.sleep(10)
             except NoSuchElementException:
                 print("Scraping terminated before reaching target number of jobs. Needed {}, got {}.".format(num_jobs, len(jobs_for_country)))
                 break
         for i in jobs_for_country:
             jobs_for_countries.append(i) 
     #This line converts the dictionary object into a pandas DataFrame.                
-    return pd.DataFrame(jobs_for_countries)                    
+    return pd.DataFrame(jobs_for_countries)    
 df=get_jobs('data',3)
 df.to_excel("data_final.xlsx",index=True) 
      
