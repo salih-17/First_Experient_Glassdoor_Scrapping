4441efa014545eb15063537fde08ec46d7a58b99
Mohammed Zwaib <46110270+Mohammed-Rwelly@users.noreply.github.com>
2022-08-20 12:37:33 -0700

    Update glasddoor_scrapping_last_version.py

--- glasddoor_scrapping_last_version.py
+++ glasddoor_scrapping_last_version.py
@@ -9,7 +9,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from datetime import datetime,timedelta
 import re
-locations = ["Qatar","United Kingdom","Teresina","João Pessoa","Aracaju","Berlin","Hamburg","Munich","Szolnok","Sopron","Stans","Zug","Dubai",'Abu Dhabi',"Sharjah"]
+locations = ["Qatar","United Kingdom","Teresina","João Pessoa","Aracaju","Berlin","Hamburg","Munich","Szolnok","Sopron","Stans","Dubai",'Abu Dhabi',"Sharjah"]
 #locations = ["Qatar","United Kingdom"]
 def get_jobs(keyword, num_jobs):
     '''Gathers jobs as a dataframe, scraped from Glassdoor'''
